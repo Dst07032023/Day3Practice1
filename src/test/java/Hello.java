6797488b0532bbcc786031cd8dfd68694e6f8e77
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-03 17:00:44 +0200

    added additional hello enhancement

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
 
-        3System.out.println("Hello World");;
+        System.out.println("Hello World");;
+        //enhancing hello
+
     }
 }
